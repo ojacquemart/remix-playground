27ca4f762db62ea3ebf414644cfcc5dc005368b6
Olivier Jacquemart <o.jacquemart@gmail.com>
2022-11-21 22:39:43 +0100

    style(auth): make auth outlet fit full width on small devices

--- app/routes/__auth.tsx
+++ app/routes/__auth.tsx
@@ -10,9 +10,7 @@ export default function AuthRoot() {
   return (
     <div className="flex flex-col justify-start items-center py-8 px-4 sm:px-0">
       <h1 className="mb-4 sm:mb-2 text-center text-4xl uppercase">{t('title')}</h1>
-      <div className="p-4 sm:p-8 w-auto sm:w-128
-          bg-white text-black
-          border-0 sm:border-2 border-solid border-zinc-400 rounded">
+      <div className="p-4 sm:p-8 w-full sm:w-128 bg-white text-black border-0 sm:border-2 border-solid border-zinc-400 rounded">
         <Outlet/>
       </div>
       <LangSelector currentLang={i18n.resolvedLanguage}></LangSelector>
