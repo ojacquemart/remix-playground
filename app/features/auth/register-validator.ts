76e8d66a978839912948f5df015be0dc7e4dc51e
Olivier Jacquemart <o.jacquemart@gmail.com>
2022-11-21 22:57:04 +0100

    fix(register): fix register schema and validation messages

--- app/features/auth/register-validator.ts
+++ app/features/auth/register-validator.ts
@@ -2,13 +2,16 @@ import { withZod } from '@remix-validated-form/with-zod';
 
 import { z } from 'zod';
 
-import { loginSchema } from '~/features/auth/login-validator';
-
-const registerSchema = loginSchema.extend(
-  {
-    passwordConfirm: z
-      .string(),
-  })
+const registerSchema = z.object({
+  email: z
+    .string()
+    .min(1, {message: 'auth.register.validator.email'})
+    .email('auth.invalidEmail'),
+  password: z
+    .string()
+    .min(8, {message: 'auth.register.validator.password'}),
+  passwordConfirm: z.string(),
+})
   .refine(
     ({password, passwordConfirm}) =>
       password === passwordConfirm,
@@ -23,6 +26,7 @@ export const createRegisterValidator = () => withZod(registerSchema);
 const usernameSchema = z.object({
   username: z
     .string()
-    .min(1, {message: 'auth.register.username.required'})});
+    .min(1, {message: 'auth.register.username.required'}),
+});
 
 export const createUsernameValidator = () => withZod(usernameSchema);
